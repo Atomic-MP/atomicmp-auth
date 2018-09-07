d944c70d1df14766810ffe1da53de90f3bfedc98
Miles <miles.b.flo@gmail.com>
2018-09-07 01:38:26 -0700

    Abstracting roles to constant

--- src/services/database/migrations/1_roles.js
+++ src/services/database/migrations/1_roles.js
@@ -1,8 +1,11 @@
-exports.up = (knex, Promise) => {
-  return knex.schema.createTable('roles', table => {
+const { ROLES } = require('../../../utils/constants')
+
+exports.up = async (knex, Promise) => {
+  await knex.schema.createTable('roles', table => {
     table.increments('role_id').primary();
     table.string('role_name', 20).notNullable();
   });
+  await knex('roles').insert(ROLES);
 };
 
 exports.down = (knex, Promise) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,28 @@ const constants = {
   SALT_ROUNDS: 10,
   HEADS: 10,
   HAIRS: 24,
-  HAIR_COLORS: 7
+  HAIR_COLORS: 7,
+  ROLES: [{
+      role_id: 1,
+      role_name: 'banned'
+    },
+    {
+      role_id: 2,
+      role_name: 'kicked'
+    },
+    {
+      role_id: 3,
+      role_name: 'user'
+    },
+    {
+      role_id: 4,
+      role_name: 'moderator'
+    },
+    {
+      role_id: 5,
+      role_name: 'admin'
+    }
+  ]
 };
 
 module.exports = constants;
