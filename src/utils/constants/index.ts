c8ed1350e0924fd7461daf2f3dbe77554f11ff6f
Miles <miles.b.flo@gmail.com>
2019-03-18 18:09:02 -0700

    Finally got this inventory thing working

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,37 @@
+const ITEMS = require('./items');
+
+const constants = {
+  TITLE: 'Atomic MP - A Multiplayer Fallout Roleplay Experience',
+  SALT_ROUNDS: 10,
+  HEADS: 10,
+  HAIRS: 24,
+  HAIR_COLORS: 7,
+  ROLES: [
+    {
+      role_id: 1,
+      role_name: 'banned',
+    },
+    {
+      role_id: 2,
+      role_name: 'kicked',
+    },
+    {
+      role_id: 3,
+      role_name: 'user',
+    },
+    {
+      role_id: 4,
+      role_name: 'moderator',
+    },
+    {
+      role_id: 5,
+      role_name: 'admin',
+    },
+  ],
+  ITEMS,
+  INVENTORY_DELIMITER: '|',
+};
+
+console.log(constants);
+
+export default constants;
