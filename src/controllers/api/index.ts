66804f0182c90a622ca5e6eaf110ae521f2e9b73
Miles <miles.b.flo@gmail.com>
2019-03-20 18:09:06 -0700

    Converting inline save state validation to class

--- src/controllers/api/index.ts
+++ src/controllers/api/index.ts
@@ -9,11 +9,7 @@ import {
   HEADS,
   ITEMS,
 } from "../../utils/constants";
-const STARTING_COORDS = {
-  x: 69449.953125,
-  y: -26285.0,
-  z: -5968.092285,
-};
+import SaveData from "./SaveData";
 const router = Router();
 router.use(protectedRoute);
 
@@ -42,7 +38,7 @@ router.get("/sample-user-data", (req, res) => {
 
 router.put("/save", async (req, res) => {
   const user = req.user;
-  let {
+  const {
     health,
     hunger,
     thirst,
@@ -51,57 +47,9 @@ router.put("/save", async (req, res) => {
     z_pos,
     inventory,
     money,
-  } = req.body;
+  } = new SaveData(req.body);
 
-  /**
-   * Save state transactions are **super fragile**
-   * Instead of dropping a malformed payload, massage into
-   * default values.
-   * This *probably* won't be abused, since the default values are shite
-   * ex: 0 money, 0 items, 0 food and water.
-   * No one would want this.
-   */
-  if (typeof health !== "number" || health < 0 || health > 100) {
-    logger.debug("[SAVE] malformed field health:" + health);
-    health = 1;
-  }
-  if (typeof hunger !== "number" || hunger < 0 || hunger > 100) {
-    logger.debug("[SAVE] malformed field hunger:" + hunger);
-    hunger = 0;
-  }
-  if (typeof thirst !== "number" || thirst < 0 || thirst > 100) {
-    logger.debug("[SAVE] malformed field thirst:" + thirst);
-    thirst = 0;
-  }
-  if (typeof x_pos !== "number") {
-    logger.debug("[SAVE] malformed field x_pos:" + x_pos);
-    x_pos = STARTING_COORDS.x;
-  }
-  if (typeof y_pos !== "number") {
-    logger.debug("[SAVE] malformed field y_pos:" + y_pos);
-    y_pos = STARTING_COORDS.y;
-  }
-  if (typeof z_pos !== "number") {
-    logger.debug("[SAVE] malformed field z_pos:" + z_pos);
-    z_pos = STARTING_COORDS.z;
-  }
-  if (!Array.isArray(inventory)) {
-    logger.debug("[SAVE] malformed field inventory:" + inventory);
-    inventory = [];
-  }
-  if (typeof money !== "number" || money < 0) {
-    money = 0;
-  }
   logger.info(`inventory: ${JSON.stringify(inventory)}, money: ${money}`);
-
-  // TODO: revisit me with .reduce statement
-  // inventory = inventory;
-  // .map(item => {
-  //   if (ITEMS.has(item.id)) {
-  //     return item;
-  //   }
-  // })
-  // .filter(Boolean);
 
   await db("users")
     .where("user_id", user.user_id)
@@ -116,7 +64,10 @@ router.put("/save", async (req, res) => {
       z_pos,
     });
 
-  logger.info(`${user.username} data saved`);
+  logger.info(`${user.username} data saved:
+  ${JSON.stringify(inventory)}
+  ${money}
+  `);
   res.sendStatus(200);
 });
 
@@ -140,18 +91,18 @@ router.put("/set-appearance", async (req, res) => {
     res.send(createError(400, "Nickname too long"));
     return;
   }
-  if (head < 1 > HEADS) {
+  if (head < 1 || head > HEADS) {
     logger.error(`[SET-APPEARANCE][ERROR] Head ${head} is malformed`);
     res.send(createError(400, `Head ${head} is malformed`));
     return;
   }
 
-  if (hair < 1 > HAIRS) {
+  if (hair < 1 || hair > HAIRS) {
     logger.error(`[SET-APPEARANCE][ERROR] Hair ${hair} is malformed`);
     res.send(createError(400, `Hair ${hair} is malformed`));
     return;
   }
-  if (hair_color < 1 > HAIR_COLORS) {
+  if (hair_color < 1 || hair_color > HAIR_COLORS) {
     logger.error(
       `[SET-APPEARANCE][ERROR] Hair Color ${hair_color} is malformed`,
     );
