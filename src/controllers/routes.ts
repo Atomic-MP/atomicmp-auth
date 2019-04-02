c06d632fe7e8083fb7200f9376c6b5305fd6aff0
Miles <miles.b.flo@gmail.com>
2019-04-02 02:32:49 -0700

    Gutting out deprecated get endpoints

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,9 +1,4 @@
 import { Router } from "express";
-import createError from "http-errors";
-import first from "lodash.first";
-
-import User from "../models/User";
-import { db, logger } from "../services";
 
 import apiRoutes from "./api";
 import loginRoutes from "./login";
@@ -25,39 +20,6 @@ router.get("/me", async (req, res) => {
 
 router.get("/ping", async (req, res) => {
   res.status(200).send("pong!");
-});
-
-router.get("/user/:id", async (req, res) => {
-  if (req.isAuthenticated()) {
-    const targetUserID = req.params.id;
-    const targetUser: User | undefined = first(await db("users")
-      .join("roles", "users.role", "=", "roles.role_id")
-      .select("user_id", "username", "role_name", "faction", "created_at")
-      .where("user_id", targetUserID));
-    if (targetUser) {
-      delete targetUser.hash;
-      res.status(200).send(targetUser);
-    } else {
-      res.send(createError(404, "User not found"));
-    }
-  } else {
-    res.redirect("/");
-  }
-});
-
-router.get("/faction/:id", async (req, res) => {
-  if (req.isAuthenticated()) {
-    const targetFactionID: number = req.params.id;
-    const faction = first(await db("factions").where("faction_id", targetFactionID));
-    if (faction) {
-      logger.info(faction);
-      res.status(200).send(faction);
-    } else {
-      res.send(createError(404, "Faction not found"));
-    }
-  } else {
-    res.redirect("/");
-  }
 });
 
 router.get("/", (req, res) => {
