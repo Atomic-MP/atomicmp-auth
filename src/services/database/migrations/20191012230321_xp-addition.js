9cc3e5b4636d6e8360d9b51d0b9ba0d053fd7ae7
CalhounDog <morgan.mcmillin.loic@gmail.com>
2019-10-13 00:37:00 -0400

    fixed a syntax error

--- src/services/database/migrations/20191012230321_xp-addition.js
+++ src/services/database/migrations/20191012230321_xp-addition.js
@@ -2,11 +2,11 @@
 exports.up = function(knex, Promise) {
   knex.schema.alterTable('users', function(t) {
 	  t.integer('xp').notNullable().defaultTo(0);
-  }
+  });
 };
 
 exports.down = function(knex, Promise) {
   knex.schema.alterTable('users', function(t) {
 	  t.dropColumn('xp');
-  }
+  });
 };
