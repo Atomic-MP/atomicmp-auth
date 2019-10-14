90a5dfea90687c77503faa318bb469e32e0580cc
Miles <miles.b.flo@gmail.com>
2019-10-14 00:19:34 -0700

    Fixing migration errors

--- src/services/database/migrations/20191012230321_xp-addition.js
+++ src/services/database/migrations/20191012230321_xp-addition.js
@@ -1,13 +1,28 @@
+const USERS_TABLE_NAME = 'users';
+const XP_COLUMN_NAME = 'xp';
+
 exports.up = function(knex) {
-  return knex.schema.alterTable('users', function(t) {
-    t.integer('xp')
-      .notNullable()
-      .defaultTo(0);
-  });
+  return knex.schema
+    .hasColumn(USERS_TABLE_NAME, XP_COLUMN_NAME)
+    .then(xpExists => {
+      if (!xpExists) {
+        return knex.schema.alterTable(USERS_TABLE_NAME, function(t) {
+          t.integer(XP_COLUMN_NAME)
+            .notNullable()
+            .defaultTo(0);
+        });
+      }
+    });
 };
 
 exports.down = function(knex) {
-  return knex.schema.alterTable('users', function(t) {
-    t.dropColumn('xp');
-  });
+  return knex.schema
+    .hasColumn(USERS_TABLE_NAME, XP_COLUMN_NAME)
+    .then(xpExists => {
+      if (xpExists) {
+        return knex.schema.alterTable(USERS_TABLE_NAME, function(t) {
+          t.dropColumn(XP_COLUMN_NAME);
+        });
+      }
+    });
 };
