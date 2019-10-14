46426a26e88e87d0effd5f6d3d06c77420fcea0c
Miles <miles.b.flo@gmail.com>
2019-10-13 23:20:16 -0700

    Fixed syntax error for reals this time

--- src/services/database/migrations/20191012230321_xp-addition.js
+++ src/services/database/migrations/20191012230321_xp-addition.js
@@ -1,12 +1,13 @@
-
-exports.up = function(knex, Promise) {
-  knex.schema.alterTable('users', function(t) {
-	  t.integer('xp').notNullable().defaultTo(0);
+exports.up = function(knex) {
+  return knex.schema.alterTable('users', function(t) {
+    t.integer('xp')
+      .notNullable()
+      .defaultTo(0);
   });
 };
 
-exports.down = function(knex, Promise) {
-  knex.schema.alterTable('users', function(t) {
-	  t.dropColumn('xp');
+exports.down = function(knex) {
+  return knex.schema.alterTable('users', function(t) {
+    t.dropColumn('xp');
   });
 };
